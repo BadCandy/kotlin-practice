5d43a2d114c914249f9dc24f6c57c1744540484d
BadCandy <christ99791@gmail.com>
2019-07-01 22:00:01 +0900

    feat(package): Practice making package.

--- src/Rectangle.kt
+++
@@ -1,9 +0,0 @@
-class Rectangle(val height: Int, val width: Int) {
-    val isSquare: Boolean
-    get() = height == width
-}
-
-val rectangle = Rectangle(1, 1)
-fun main(args: Array<String>) {
-    println("${rectangle.isSquare}")
-}
